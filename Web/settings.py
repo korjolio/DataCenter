f0fcc098732add61935f53ba2e63f839e4f4b9b2
pepo <ppbugueno@gmail.com>
2020-10-17 19:31:50 -0300

    Cambio a base de datos MySQL, creación del superusuario, servicios, posts y categorias

--- Web/settings.py
+++ Web/settings.py
@@ -78,11 +78,15 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'web_datacenter_bd',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
