34ba7d73ef3d4f4a321d95c8fd07b38b2cd531fb
pepo <ppbugueno@gmail.com>
2020-11-03 16:56:50 -0300

    actualizacion tests.py

--- WebApp/tests.py
+++ WebApp/tests.py
@@ -4,20 +4,20 @@ from WebApp.models import Pedido
 # Create your tests here.
 class ProductoTestCase(TestCase):
     def setUp(self):
-        Pedido.objects.create(id=1, rut="123123" , cert_ssl=1, dominio="www.ejemplo.com", email="ejemplo@host")
+        Pedido.objects.create(id=1, rut="2361910-5", tipo_plan=2, vigencia=2, cert_ssl=1, dominio="http://www.ejemplo.com", email="ejemplo@host.com")
 
-def test_pedidoingresadorut(self):
-    producto1 = Pedido.objects.get(id=1)
-    self.assertEqual(producto1.rut,"123123")
+    def test_pedidoingresadorut(self):
+        producto1 = Pedido.objects.get(id=1)
+        self.assertEqual(producto1.rut,"2361910-5")
 
-def test_pedidoingresadocert(self):
-    producto1 = Pedido.objects.get(id=1)
-    self.assertEqual(producto1.cert_ssl,1)
+    def test_pedidoingresadocert(self):
+        producto1 = Pedido.objects.get(id=1)
+        self.assertEqual(producto1.cert_ssl,1)
 
-def test_pedidoingresadodominio(self):
-    producto1 = Pedido.objects.get(id=1)
-    self.assertEqual(producto1.dominio,"www.ejemplo.com") 
+    def test_pedidoingresadodominio(self):
+        producto1 = Pedido.objects.get(id=1)
+        self.assertEqual(producto1.dominio,"http://www.ejemplo.com") 
 
-def test_pedidoingresamail(self):
-    producto1 = Pedido.objects.get(id=1)
-    self.assertEqual(producto1.email,"ejemplo@host.com")   
+    def test_pedidoingresamail(self):
+        producto1 = Pedido.objects.get(id=1)
+        self.assertEqual(producto1.email,"ejemplo@host.com")   
