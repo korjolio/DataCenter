b80704bb8dc7f5e82a53c1d9c81a1c67cb002560
pepo <ppbugueno@gmail.com>
2020-11-03 17:29:35 -0300

    Actualizacion del tests.py

--- WebApp/tests.py
+++ WebApp/tests.py
@@ -4,7 +4,7 @@ from WebApp.models import Pedido
 # Create your tests here.
 class ProductoTestCase(TestCase):
     def setUp(self):
-        Pedido.objects.create(id=1, rut="2361910-5", tipo_plan=2, vigencia=2, cert_ssl=1, dominio="http://www.ejemplo.com", email="ejemplo@host.com")
+        Pedido.objects.create(id=1, rut="2361910-5", tipo_plan=2, vigencia=2, cert_ssl=1, dominio="http://www.ejemplo.com", email="ejemplohost.com")
 
     def test_pedidoingresadorut(self):
         producto1 = Pedido.objects.get(id=1)
