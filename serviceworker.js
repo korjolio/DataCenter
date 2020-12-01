1bda5b24bc98fcc2d21517db30f6a1e4588c48bc
Pepo <ppbugueno@gmail.com>
2020-12-01 18:22:04 -0300

    Proyecto finalizado

--- serviceworker.js
+++ serviceworker.js
@@ -57,20 +57,6 @@ self.addEventListener('activate', event => {
     );
 });
 
-// Serve from Cache
-/*
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-            .catch(() => {
-                return caches.match('/offline/');
-            })
-    )
-});
-*/
 
 self.addEventListener("fetch", function(event) {
     event.respondWith(
